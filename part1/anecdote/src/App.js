2d784618510502df1fa6e5591946ddfdcfda7e36
Gio <giofra.dev@gmail.com>
2023-03-07 15:05:55 +0200

    part1.12 anecdotes

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -14,7 +14,18 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  const handleClick = () => {
+    const randomAnecdote = Math.floor(Math.random() * anecdotes.length);
+    console.log(randomAnecdote);
+    setSelected(randomAnecdote);
+  };
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>next anecdote</button>
+    </div>
+  );
 };
 
 export default App;
