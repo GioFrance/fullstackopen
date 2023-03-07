f41b789c7ba2b8d16d7e7687bd5e7b0994a7be94
Gio <giofra.dev@gmail.com>
2023-03-07 12:39:23 +0200

    part1.7 and part 1.8 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,18 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>total: {props.total}</p>
+      <p>average: {props.average}</p>
+      <p>percentage: {props.percentage}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -20,6 +33,10 @@ const App = () => {
     setBad(updateBad);
   };
 
+  const total = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / total;
+  const percentage = (good / total) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,11 +44,14 @@ const App = () => {
       <button onClick={handleClickNeutral}> neutral </button>
       <button onClick={handleClickBad}> bad </button>
       <h1>statistics</h1>
-      <div>
-        <p>good : {good}</p>
-        <p>neutral : {neutral}</p>
-        <p>bad : {bad}</p>
-      </div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        percentage={percentage}
+      />
     </div>
   );
 };
