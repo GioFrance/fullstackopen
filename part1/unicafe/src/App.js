4db5092679c262f301db7e5a56f03dff2970c10e
Gio <giofra.dev@gmail.com>
2023-03-07 12:44:49 +0200

    part1.9 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
   return (
     <div>
       <p>good: {props.good}</p>
