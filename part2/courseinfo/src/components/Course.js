d50d5da023df0346647054139af1d7e89527b946
Gio <giofra.dev@gmail.com>
2023-03-09 13:36:02 +0200

    part2.3 Reduce Total and Refactored more courseinfo

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,7 +1,7 @@
 const Header = ({ courseName }) => {
   return (
     <div>
-      <h1>{courseName.name}</h1>
+      <h1>{courseName}</h1>
     </div>
   );
 };
@@ -19,21 +19,22 @@ const Part = ({ coursePartsFinal }) => {
 const Content = ({ courseParts }) => {
   return (
     <div>
-      <Part coursePartsFinal={courseParts.parts[0]} />
-      <Part coursePartsFinal={courseParts.parts[1]} />
-      <Part coursePartsFinal={courseParts.parts[2]} />
+      <Part coursePartsFinal={courseParts[0]} />
+      <Part coursePartsFinal={courseParts[1]} />
+      <Part coursePartsFinal={courseParts[2]} />
     </div>
   );
 };
 
 const Total = ({ courseTotal }) => {
+  const total = courseTotal.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+  console.log({ total });
   return (
     <div>
       <p>
-        <b>Number of exercises </b>
-        {courseTotal.parts[0].exercises +
-          courseTotal.parts[1].exercises +
-          courseTotal.parts[2].exercises}
+        <b>Number of exercises = {total} </b>
       </p>
     </div>
   );
@@ -42,9 +43,9 @@ const Total = ({ courseTotal }) => {
 const Course = ({ course }) => {
   return (
     <div>
-      <Header courseName={course} />
-      <Content courseParts={course} />
-      <Total courseTotal={course} />
+      <Header courseName={course.name} />
+      <Content courseParts={course.parts} />
+      <Total courseTotal={course.parts} />
     </div>
   );
 };
