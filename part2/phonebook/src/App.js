fa64aa1e8ac88a46a005ca147a7a77aeb8191f14
Gio <giofra.dev@gmail.com>
2023-03-15 17:26:24 +0200

    part2.11 Effect-hooks and db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import H1Header from "./components/H1Header";
 import H2Header from "./components/H2Header";
 import Filter from "./components/Filter";
@@ -6,16 +7,20 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("...type a name");
   const [number, setNumber] = useState("");
   const [showFilter, setShowFilter] = useState("");
   const titleMain = "Phonebook";
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addPhonebook = (event) => {
     event.preventDefault();
