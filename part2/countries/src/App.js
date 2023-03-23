62502a1fd14f4699707474d6854cf75f5c6c0010
Gio <giofra.dev@gmail.com>
2023-03-23 21:37:57 +0200

    part2.18 added filter

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [countriesToShow, setCountriesToShow] = useState([]);
 
-  axios.get("https://restcountries.com/v3.1/all").then((response) => {
-    setCountries(response.data);
-  });
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+    setCountriesToShow(
+      countries.filter((country) =>
+        country.name.common.toLowerCase().includes(filter.toLowerCase())
+      )
+    );
+  };
 
   return (
     <div>
       <h1>Countries</h1>
       <div>
-        name of country: <input />
+        name of country: <input value={filter} onChange={handleFilter} />
       </div>
-      <ul>
+      <div>
         {countries.map((country) => (
           <li key={country.name.official}>{country.name.common}</li>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
