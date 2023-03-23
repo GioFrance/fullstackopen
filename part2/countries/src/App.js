eca0cf63ec15ffe7c6c1d3873471a4f4b45f143f
Gio <giofra.dev@gmail.com>
2023-03-23 21:11:57 +0200

    part2.18 list all countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  axios.get("https://restcountries.com/v3.1/all").then((response) => {
+    setCountries(response.data);
+  });
+
   return (
     <div>
       <h1>Countries</h1>
       <div>
         name of country: <input />
       </div>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name.official}>{country.name.common}</li>
+        ))}
+      </ul>
     </div>
   );
 };
